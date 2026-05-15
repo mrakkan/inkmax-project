440d02efb2c28799cf5b7ab4011c8d1afcd725a2
mrakkan <mansuperyoung4399@gmail.com>
2026-05-15 19:47:25 +0700

    fix css problem

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,15 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
-  // 1. ถ้า URL คือ https://<username>.github.io/<repository-name>/
-  // ต้องใส่ชื่อ repo ใน basePath ด้วย (เช่น '/my-nextjs-site')
-  basePath: isProd ? "/inkmax-project" : "",
-  assetPrefix: isProd ? "/inkmax-project/" : "",
-
-  // 2. แนะนำให้ใส่ trailingSlash เพื่อป้องกันปัญหา 404 เมื่อกด Refresh หน้าอื่นที่ไม่ใช่หน้าแรก
+  
+  // แนะนำให้ใส่ trailingSlash เวลา export เป็น static html
   trailingSlash: true,
 
-  // 3. GitHub Pages ไม่มี Server สำหรับจัดการรูปภาพ
+  // ตอน export เป็น static ต้องใช้ unoptimized images เว้นแต่จะใช้ image service แยก
   images: {
     unoptimized: true,
   },
 };
-
-module.exports = nextConfig
 
 export default nextConfig;
