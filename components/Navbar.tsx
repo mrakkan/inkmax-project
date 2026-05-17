c30f9226438d9f0184ee865f935c77ef6424ff85
mrakkan <mansuperyoung4399@gmail.com>
2026-05-17 07:41:30 +0700

    merge error fix navbar icon import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useMemo, useRef, useState } from "react";
